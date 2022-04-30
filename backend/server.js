0eb327260ac557315c6a5232c2c54a7c3f96542c
sadra <salah.fattahi@gmail.com>
2022-04-30 18:59:29 +0430

    routing start

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,23 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const port = 4000
 
+const userRoute = express.Router()
+
 
 app.use(cors())
 app.use(bodyParser.json())
 
-app.post('/', (req, res) => {
-    res.send('Hello World!')
+app.use(function (req, res, next) {
+    console.log("this is an todo app")
+    next()
 })
+
+userRoute.route("/:name/:id").get(function (req, res) {
+    res.send({ "name": req.params.name, "id": req.params.id })
+})
+
+
+app.use("/user", userRoute)
 
 app.listen(port, () => {
     console.log(`server listening on port ${port}`)
